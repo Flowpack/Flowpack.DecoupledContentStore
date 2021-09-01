c19f3a856fff9994b3ebdf0a7e5af4ebb1b8d813
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2021-09-01 17:00:42 +0200

    BUGFIX: fix renderWorker to allow termination

--- Scripts/renderWorker.sh
+++ Scripts/renderWorker.sh
@@ -3,8 +3,39 @@
 CONTENTRELEASE=$1
 WORKER=$2
 
+# taken from https://newbedev.com/forward-sigterm-to-child-in-bash
+prep_term()
+{
+    unset term_child_pid
+    unset term_kill_needed
+    trap 'handle_term' TERM INT
+}
+
+handle_term()
+{
+    if [ "${term_child_pid}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null
+    else
+        term_kill_needed="yes"
+    fi
+}
+
+wait_term()
+{
+    term_child_pid=$!
+    if [ "${term_kill_needed}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null
+    fi
+    wait ${term_child_pid} 2>/dev/null
+    trap - TERM INT
+    wait ${term_child_pid} 2>/dev/null
+}
+
+
 while true; do
-  ./flow nodeRendering:renderWorker $CONTENTRELEASE $WORKER
+  prep_term
+  ./flow nodeRendering:renderWorker $CONTENTRELEASE $WORKER &
+  wait_term
   RETURNCODE=$?
   if [ $RETURNCODE -ne 193 ]
   then
