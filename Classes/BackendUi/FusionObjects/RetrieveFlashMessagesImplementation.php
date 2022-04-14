80a2222633990a2eafdb33c2d3714e33e659ac43
batabana <batabana-github@systemli.org>
2022-04-14 09:17:36 +0200

    TASK: refactor retrieve flash message implementation

--- Classes/BackendUi/FusionObjects/RetrieveFlashMessagesImplementation.php
+++ Classes/BackendUi/FusionObjects/RetrieveFlashMessagesImplementation.php
@@ -5,7 +5,6 @@ namespace Flowpack\DecoupledContentStore\BackendUi\FusionObjects;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Mvc\FlashMessage\FlashMessageService;
 use Neos\Fusion\FusionObjects\AbstractFusionObject;
-use Neos\Error\Messages\Message;
 
 class RetrieveFlashMessagesImplementation extends AbstractFusionObject
 {
@@ -17,12 +16,6 @@ class RetrieveFlashMessagesImplementation extends AbstractFusionObject
 
     public function evaluate()
     {
-        $messages = $this->flashMessageService->getFlashMessageContainerForRequest($this->runtime->getControllerContext()->getRequest())->getMessagesAndFlush();
-        return array_map(function (Message $message) {
-            return [
-                'message' => $message->getMessage(),
-                'severity' => $message->getSeverity()
-            ];
-        }, $messages);
+        return $this->flashMessageService->getFlashMessageContainerForRequest($this->runtime->getControllerContext()->getRequest())->getMessagesAndFlush();
     }
 }
