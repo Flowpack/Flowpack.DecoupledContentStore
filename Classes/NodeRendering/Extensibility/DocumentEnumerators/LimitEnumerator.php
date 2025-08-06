e5259685d7c27131e4737c41d201f9cd4a531a89
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2025-08-06 10:54:42 +0200

    TASK: improve LimitEnumerator for debugging

--- Classes/NodeRendering/Extensibility/DocumentEnumerators/LimitEnumerator.php
+++ Classes/NodeRendering/Extensibility/DocumentEnumerators/LimitEnumerator.php
@@ -13,17 +13,29 @@ use Neos\ContentRepository\Domain\Model\NodeInterface;
 class LimitEnumerator implements DocumentEnumeratorInterface
 {
     protected int $i = 0;
-    private int $limit;
+    private ?int $limit = null;
+    private ?string $uriPathSegmentFilter = null;
 
     public function __construct(
         array $options = []
     ) {
-        $this->limit = $options['limit'] ?? throw new \InvalidArgumentException('Missing limit option');
+        $this->limit = $options['limit'] ?? null;
+        $this->uriPathSegmentFilter = $options['uriPathSegmentFilter'] ?? null;
     }
     public function enumerateDocumentNode(NodeInterface $documentNode): iterable
     {
-        if ($this->i++ >= $this->limit) {
-            return [];
+        if ($this->uriPathSegmentFilter !== null) {
+            if (str_contains($documentNode->getProperty('uriPathSegment'), $this->uriPathSegmentFilter) === false) {
+                return [];
+            }
+        }
+
+        // NOTE: Limiting must come LAST, after all other constraints have been evaluated (because we want
+        // only filtered nodes from the other conditions to be counted against the limit)
+        if ($this->limit !== null) {
+            if ($this->i++ >= $this->limit) {
+                return [];
+            }
         }
 
         return [
