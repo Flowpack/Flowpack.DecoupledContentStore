5d95d6350622c3637381cb4b07aa10ebe672e282
Timon Heuser <timon.heuser@sandstorm.de>
2026-05-08 15:11:09 +0200

    simplify code

--- Classes/BackendUi/Dto/WorkerErrorLog.php
+++ Classes/BackendUi/Dto/WorkerErrorLog.php
@@ -11,78 +11,21 @@ use Neos\Flow\Annotations as Flow;
 /**
  * @Flow\Proxy(false)
  */
-final class WorkerErrorLog implements ProtectedContextAwareInterface
+final class WorkerErrorLog
 {
-    private string $workerName;
-    private string $status;
-    private int $exitCode;
-    private ?string $taskError;
-    private ?string $lastAttemptedNode;
-
-    /**
-     * @var string[]
-     */
-    private array $errorBlocks;
+    public bool $wasKilledByOrchestrator;
 
     /**
      * @param string[] $errorBlocks
      */
     public function __construct(
-        string $workerName,
-        string $status,
-        int $exitCode,
-        ?string $taskError,
-        array $errorBlocks,
-        ?string $lastAttemptedNode = null
+        public string $workerName,
+        public string $status,
+        public int $exitCode,
+        public ?string $taskError,
+        public array $errorBlocks,
+        public ?string $lastAttemptedNode = null
     ) {
-        $this->workerName = $workerName;
-        $this->status = $status;
-        $this->exitCode = $exitCode;
-        $this->taskError = $taskError;
-        $this->errorBlocks = $errorBlocks;
-        $this->lastAttemptedNode = $lastAttemptedNode;
-    }
-
-    public function getWorkerName(): string
-    {
-        return $this->workerName;
-    }
-
-    public function getStatus(): string
-    {
-        return $this->status;
-    }
-
-    public function getExitCode(): int
-    {
-        return $this->exitCode;
-    }
-
-    public function getTaskError(): ?string
-    {
-        return $this->taskError;
-    }
-
-    /**
-     * @return string[]
-     */
-    public function getErrorBlocks(): array
-    {
-        return $this->errorBlocks;
-    }
-
-    public function getLastAttemptedNode(): ?string
-    {
-        return $this->lastAttemptedNode;
-    }
-
-    public function wasKilledByOrchestrator(): bool
-    {
-        return $this->exitCode === WorkerErrorLogAggregator::EXIT_CODE_SIGTERM;
-    }
-
-    public function allowsCallOfMethod($methodName): bool
-    {
-        return true;
+        $this->wasKilledByOrchestrator = $this->exitCode === WorkerErrorLogAggregator::EXIT_CODE_SIGTERM;
     }
 }
